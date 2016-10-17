8ddd30bcace17b89c4050fe8242ae9d7a3295e0b
atans2468 <austintans7@gmail.com>
2016-10-17 02:04:49 -0700

    simplifying header

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,1 @@
 var helloWorld = angular.module('helloWorld.controllers', []);
-
-//controller to track which state is active so we can inject that into the header
-helloWorld.controller("headerCtrl", function($scope, $location) {
-  $scope.isActive = function (viewLocation) { 
-        return viewLocation === $location.path();
-    };
-  $('.navbar-collapse a:not(.dropdown-toggle)').click(function(){
-    $(".navbar-collapse").collapse('hide');
-  });
-});
